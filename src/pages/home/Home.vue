d93e91093aa1796d0f6b90b2fea2332893b3533b
kotori2000 <2577559811@qq.com>
2019-04-01 20:10:05 +0800

    小的初始化

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        hello
+    </div>
+</template>
+<script>
+/* eslint-disable */ 
+export default {
+    name: 'Home'
+}
+</script>
+<style lang='scss' scoped>
+</style>
