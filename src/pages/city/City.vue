a9513e171a475154c125195deece22301b31a985
kotori2000 <2577559811@qq.com>
2019-04-03 20:49:44 +0800

    城市列表

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,24 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-Alpahbet></city-Alpahbet>
     </div>
 </template>
 <script>
 /* eslint-disable */
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlpahbet from './components/Alpahbet'
 export default {
   name: 'city',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlpahbet
+
   }
 }
 </script>
